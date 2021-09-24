e853fb7efa0ab4b503d13114141647dd883deef2
Arnold <arnold.nowik@protonmail.com>
2021-09-24 13:53:36 +0700

    Add some more scripts

--- awscliv2setup.sh
+++ awscliv2setup.sh
@@ -3,7 +3,3 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 export PATH=$PATH:/usr/local/bin/aws
-echo "=============================="
-echo "Zaktualizuj NTP server!!!!!!"
-echo "'aws s3 cp nazwapliku s3://bucket_name --storage-class DEEP_ARCHIVE'"
-echo "'aws s3api list-objects-v2 --bucket bucket_name'"
